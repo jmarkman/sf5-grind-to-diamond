b49b87188647b1e27657706af743abe71db04453
jmarkman <jmarkman446@gmail.com>
2023-09-02 17:39:43 -0400

    add state to SiteBody for switching graph type

--- src/components/sitebody.tsx
+++ src/components/sitebody.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { RankedVisualizationType } from '../models/RankedVisualizationType';
 
 const SiteBody = () => {
+    const [rankedVisualizationType, setRankedVisualizationType] = useState<RankedVisualizationType>(RankedVisualizationType.PerSession);
+
+    const changeVisualizationType = (event: React.MouseEvent<HTMLButtonElement>) => {
+        let visualizationButton: HTMLButtonElement = event.currentTarget;
+
+        if (rankedVisualizationType === visualizationButton.value) {
+            return;
+        }
+
+        setRankedVisualizationType(visualizationButton.value as RankedVisualizationType);
+    }
+
     return (
         <div className='container'>
             <div className='row'>
                 <div className='col d-flex justify-content-center'>
-                    <button type="button" className="btn btn-primary">LP change by session date</button>
+                    <button type="button" onClick={changeVisualizationType} value={RankedVisualizationType.PerSession} className="btn btn-primary">LP change per session</button>
                 </div>
                 <div className='col d-flex justify-content-center'>
-                    <button type="button" className="btn btn-primary">LP change for all games</button>
+                    <button type="button" onClick={changeVisualizationType} value={RankedVisualizationType.AllGames} className="btn btn-primary">LP change for all games</button>
                 </div>
                 <div className='col d-flex justify-content-center'>
-                    <button type="button" className="btn btn-primary">Win/loss percentage</button>
+                    <button type="button" onClick={changeVisualizationType} value={RankedVisualizationType.WinLoss} className="btn btn-primary">Win/loss percentage</button>
                 </div>
                 <div className='col d-flex justify-content-center'>
-                    <button type="button" className="btn btn-primary">Character frequency</button>
+                    <button type="button" onClick={changeVisualizationType} value={RankedVisualizationType.CharFreq} className="btn btn-primary">Character frequency</button>
                 </div>
             </div>
 
---
+++ src/models/RankedVisualizationType.ts
@@ -0,0 +1,6 @@
+export enum RankedVisualizationType {
+    PerSession = "PerSession",
+    AllGames = "AllGames",
+    WinLoss = "WinLoss",
+    CharFreq = "CharFreq"
+}
