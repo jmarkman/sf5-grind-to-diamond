6be52d95d9ee000d3e716750c7637d6cfb102bc8
jmarkman <jmarkman446@gmail.com>
2023-09-07 18:29:00 -0400

    refactor per session chart for chartjs to use RankedSession object as its data object
    Refactored the PerSessionChart to encapsulate
    array building logic and use the RankedSession object
    for the data in the chartjs dataset. This revealed a small
    issue I didn't catch in my excel sheet where I wrote
    "Zangief" for most of the entries and then "Gief" for some random ones.

--- src/components/visualizations/persessionchart.tsx
+++ src/components/visualizations/persessionchart.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import VisualizationProps from '../../models/VisualizationProps';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, ChartData, ChartOptions } from 'chart.js';
 import RankedSession from '../../models/RankedSession';
@@ -8,8 +7,8 @@ const PerSessionChart = (props: VisualizationProps) => {
     ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
     let rankedData: RankedSession[] = props.data;
-    let lineChartLabels: string[] = [];
-    let perSessionPointsData: number[] = [];
+    let lineChartLabels: string[] = generateDateAxis(rankedData);
+    let allRankedSessions: RankedSession[] = generateRankedSessionData(rankedData);
     
     let chartOptions: ChartOptions = {
         responsive: true,
@@ -20,25 +19,20 @@ const PerSessionChart = (props: VisualizationProps) => {
             title: {
                 display: true,
                 text: 'LP Delta Per Ranked Session'
-            },
+            }
         },
     };
 
-    // Coalesce data (extract to own function)
-    lineChartLabels.push("2022-12-31");
-    perSessionPointsData.push(rankedData[0].pointsStart);
-
-    rankedData.forEach(x => {
-        lineChartLabels.push(x.date);
-        perSessionPointsData.push(x.pointsEnd);
-    });
-
-    let lineChartData: ChartData<"line"> = {
+    let lineChartData: ChartData<"line", RankedSession[]> = {
         labels: lineChartLabels,
         datasets: [
             {
                 label: 'Session Point Change',
-                data: perSessionPointsData,
+                data: allRankedSessions,
+                parsing: {
+                    xAxisKey: 'date',
+                    yAxisKey: 'pointsEnd'
+                },
                 borderColor: 'rgb(255, 99, 132)',
                 backgroundColor: 'rgba(255, 99, 132, 0.5)'
             }
@@ -46,6 +40,33 @@ const PerSessionChart = (props: VisualizationProps) => {
     };
 
     return <Line options={chartOptions} data={lineChartData} />
+};
+
+/**
+ * Builds the array that represents the x-axis for the line chart
+ * @remarks I construct a "fake" date to represent my ranked state from before I started tracking my progress
+ * @param data The ranked data for all my games
+ * @returns A string array with dates to be used as the x-axis for the line chart
+ */
+const generateDateAxis = (data: RankedSession[]): string[] => {
+    let labels: string[] = [];
+    labels.push('2022-12-31');
+    data.forEach(x => labels.push(x.date));
+
+    return labels;
+};
+
+/**
+ * Builds the array that contains the ranked data for the line chart
+ * @remarks I construct a "fake" RankedSession object to represent my total SF5 Ranked state before I started tracking my progress
+ * @param data The ranked data for all my games
+ * @returns An array of all my ranked games plus a placeholder at index 0 to represent the first step
+ */
+const generateRankedSessionData = (data: RankedSession[]): RankedSession[] => {
+    let initialSession: RankedSession[] = [new RankedSession("2022-12-31", data[0].pointsStart, data[0].pointsStart, [])];
+    let rankedArr: RankedSession[] = [...initialSession, ...data];
+    
+    return rankedArr;
 };
 
 export default PerSessionChart;
--- src/models/RankedSession.ts
+++ src/models/RankedSession.ts
@@ -1,8 +1,17 @@
 import RankedMatch from "./RankedMatch";
 
 export default class RankedSession {
-    date: string;
-    pointsStart: number;
-    pointsEnd: number;
+    public date: string;
+    public pointsStart: number;
+    public pointsEnd: number;
     matches: RankedMatch[];
+
+
+
+    constructor(date: string, pointsStart: number, pointsEnd: number, matches: RankedMatch[]) {
+        this.date = date;
+        this.pointsStart = pointsStart;
+        this.pointsEnd = pointsEnd;
+        this.matches = matches;
+    }
 }
