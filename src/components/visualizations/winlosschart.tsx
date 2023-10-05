1125b791a8cd76098a4823409b56e217e7c99971
jmarkman <jmarkman446@gmail.com>
2023-10-05 03:01:24 -0400

    add ability for users to see win-loss per character in addition to my entire run
    This change wraps up the functionality
    for the winlosschart visualization. I've also set
    the groundwork for being able to toggle chart
    colors here for light/dark mode, and I made some
    refactor decisions that I'll carry over to the other
    visualizations.

--- src/components/visualizations/winlosschart.tsx
+++ src/components/visualizations/winlosschart.tsx
@@ -1,95 +1,70 @@
 import React, { useState } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend, ChartOptions, ChartData } from 'chart.js';
 import VisualizationProps from "../../models/VisualizationProps";
-import RankedSession from "../../models/RankedSession";
 import RankedMatch from "../../models/RankedMatch";
 import { Pie } from "react-chartjs-2";
-import ChartDataLabels from "chartjs-plugin-datalabels";
+import ChartDataLabels, { Context } from "chartjs-plugin-datalabels";
 import WinLoss from "../../models/WinLoss";
 
 const WinLossChart = (props: VisualizationProps) => {
     ChartJS.register(ArcElement, Tooltip, Legend, ChartDataLabels);
     const ALLCHARACTERS: string = "All Characters";
+    const ALLMATCHES: RankedMatch[] = props.data.map(x => x.matches).reduce((a, b) => a.concat(b));
+    const TOTALWINS: number = ALLMATCHES.filter(x => x.result.toLowerCase() === "win").length;
+    const TOTALLOSSES: number = ALLMATCHES.filter(x => x.result.toLowerCase() === "loss").length;
     const [winLossFilter, setWinLossFilter] = useState<string>(ALLCHARACTERS);
 
-    let rankedData: RankedSession[] = props.data;
+    const changeCharacter = (event: React.ChangeEvent<HTMLSelectElement>) => {
+      setWinLossFilter(event.target.value);
+    }
 
-    // Build options (extract to own function)
     let chartOptions: ChartOptions = {
         responsive: true,
         plugins: {
             legend: {
-                position: 'top' as const,
-            },
-            title: {
-                display: true,
-                text: 'Win/Loss Ratio'
+                position: 'left' as const,
             }
         },
     };
 
-    let allMatches: RankedMatch[] = rankedData.map(x => x.matches).reduce((a, b) => a.concat(b));
+    let characterWinLoss: Map<string, WinLoss> = createCharacterWinLossMap(ALLMATCHES);
+    let backgroundColor: string[] = ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'];
+    let borderColor: string[] = ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'];
+    let resultSet: number[];
 
-    let characterWinLoss: Map<string, WinLoss> = new Map();
-
-    for (let match of allMatches) {
-      if (characterWinLoss.has(match.opponent)) {
-        let winLossDetails: WinLoss = characterWinLoss.get(match.opponent)!;
-        if (match.result.toLowerCase() === "win") {
-          characterWinLoss.set(match.opponent, { wins: winLossDetails.wins + 1, losses: winLossDetails.losses });
-        } else {
-          characterWinLoss.set(match.opponent, { wins: winLossDetails.wins, losses: winLossDetails.losses + 1 });
-        }
-      } else {
-        characterWinLoss.set(match.opponent, { wins: 0, losses: 0 });
-      }
+    switch (winLossFilter) {
+      case ALLCHARACTERS:
+        resultSet = [TOTALLOSSES, TOTALWINS];
+        break;
+      default:
+        resultSet = singleCharacterWinLossFilter(characterWinLoss.get(winLossFilter)!);
+        break;
     }
 
-    let totalWins: number = allMatches.filter(x => x.result.toLowerCase() === "win").length;
-    let totalLosses: number = allMatches.filter(x => x.result.toLowerCase() === "loss").length;
-
-    const pieChartData: ChartData<"pie"> = {
-        labels: ['Loss', 'Win'],
-        datasets: [
-          {
-            label: 'Total',
-            data: [totalLosses, totalWins],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)'       
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)'
-            ],
-            datalabels: {
-              formatter(value, context) {
-                let sum: number = 0;
-                let dataArr = context.chart.data.datasets[0].data;
-
-                dataArr.forEach((data) => {
-                  sum += data as number;
-                });
-
-                let percentage: string = `${((value * 100) / sum).toFixed(1)}%`;
-                return percentage;
-              },
-              font: {
-                weight: 'bold',
-                size: 48
-            },
-            borderWidth: 1
-          },
-        }],
-      };
+    const pieChartData: ChartData<"pie"> = generatePieChart(resultSet, backgroundColor, borderColor);
       
     return (
       <div>
         <div className="row">
           <div className="col">
-            <p>This is my win-loss ratio for all the games I played during my ranked grind.</p>
-            <p>In total, I played {allMatches.length} games; {totalWins} were wins and {totalLosses} were losses.</p>
+            <p>This is my win-loss ratio for all the games I played during my ranked grind. You can see individual character win-loss by selecting a character from the dropdown field below.</p>
+            <p>Selecting "All Characters" will return you to the default of my total win-loss ratio. Ibuki is not present in the list, see the "Character frequency" visualization for more information.</p>
           </div>
+        </div>
+
+        <div className="row py-3 align-items-center justify-content-center">
+          <div className="col">
+            <select className="form-select" aria-label="Win Loss Filter" onChange={changeCharacter}>
+              <option value={ALLCHARACTERS} selected>{ALLCHARACTERS}</option>
+              {[...characterWinLoss.keys()].map((x) => <option value={x}>{x}</option>)}
+            </select>
+          </div>
+        </div>
+
+        <div className="row">
+          { winLossFilter === ALLCHARACTERS 
+            ? <p className="d-flex pt-3 mx-auto fs-2 justify-content-center align-items-center">For my entire ranked grind run, I won {TOTALWINS} times and lost {TOTALLOSSES} times.</p> 
+            : <p className="d-flex pt-3 mx-auto fs-2 justify-content-center align-items-center">vs {winLossFilter}, I won {resultSet[1]} {resultSet[1] > 1 || resultSet[1] === 0 ? "times" : "time"} and lost {resultSet[0]} {resultSet[0] > 1 || resultSet[0] === 0 ? "times" : "time"}.</p> }
         </div>
 
         <div className="row">
@@ -99,19 +74,90 @@ const WinLossChart = (props: VisualizationProps) => {
             </div>          
           </div>
         </div>
-
-        <div className="row align-items-center justify-content-center">
-          <div className="col">
-            <select className="form-select" aria-label="Win Loss Fiter">
-              <option selected>All Characters</option>
-              <option value="Ryu">Ryu</option>
-            </select>
-          </div>
-        </div>
       </div>
     );
 };
 
+/**
+ * Generates a dataset of two values: my total match wins and losses against a specific character
+ * @remarks Like allMatchesWinLossFilter, index 0 is my losses, index 1 is my wins
+ * @param opponent The opposing character's wins and losses
+ * @returns An array containing my total wins and losses vs a specific character
+ */
+const singleCharacterWinLossFilter = (opponent: WinLoss): number[] => {
+  return [opponent.losses, opponent.wins]
+};
 
+/**
+ * Generates a mapping of characters and the number of wins and losses I had against that character
+ * @param rankedMatches An array of all of the matches I played during my grind to diamond
+ * @returns A map of characters and the number of wins and losses I had against each character
+ */
+const createCharacterWinLossMap = (rankedMatches: RankedMatch[]): Map<string, WinLoss> => {
+  let characterWinLoss: Map<string, WinLoss> = new Map();
+
+  for (let match of rankedMatches) {
+    if (characterWinLoss.has(match.opponent)) {
+      let winLossDetails: WinLoss = characterWinLoss.get(match.opponent)!;
+      if (match.result.toLowerCase() === "win") {
+        characterWinLoss.set(match.opponent, { wins: winLossDetails.wins + 1, losses: winLossDetails.losses });
+      } else {
+        characterWinLoss.set(match.opponent, { wins: winLossDetails.wins, losses: winLossDetails.losses + 1 });
+      }
+    } else {
+      if (match.result.toLowerCase() === "win") {
+        characterWinLoss.set(match.opponent, { wins: 1, losses: 0 });
+      } else {
+        characterWinLoss.set(match.opponent, { wins: 0, losses: 1 });
+      }
+    }
+  }
+
+  return characterWinLoss;
+};
+
+/**
+ * Generates the data and the associated customizations necessary for rending the win-loss pie chart
+ * @param winLossData The number of wins and losses to use for the pie chart
+ * @param customBackgroundColor Array of RGB CSS methods for the background colors for each pie chart slice
+ * @param customBorderColor Array of RGB CSS methods for the border colors for each pie chart slice
+ * @returns The data and customizations associated with the pie chart
+ */
+const generatePieChart = (winLossData: number[], customBackgroundColor: string[], customBorderColor: string[]): ChartData<"pie"> => {  
+  let chartData: ChartData<"pie"> = {
+    labels: ['Loss', 'Win'],
+    datasets: [
+      {
+        label: 'Total',
+        data: winLossData,
+        backgroundColor: customBackgroundColor,
+        borderColor: customBorderColor,
+        datalabels: {
+          formatter(value: number, context: Context) {
+            let percentage: string = "";
+            let sum: number = 0;
+            let dataArr = context.chart.data.datasets[0].data;
+
+            dataArr.forEach((data) => {
+              sum += data as number;
+            });
+
+            if (value !== 0) {
+              percentage = `${((value * 100) / sum).toFixed(1)}%`;
+            } 
+
+            return percentage;
+          },
+          font: {
+            weight: 'bold',
+            size: 48
+        },
+        borderWidth: 1
+      },
+    }],
+  };
+
+  return chartData;
+}
 
 export default WinLossChart;
