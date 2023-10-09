f69ba4411a58b1bc7cb3ecb61561df05da3a2a8e
jmarkman <jmarkman446@gmail.com>
2023-10-09 17:07:37 -0400

    revise button row for visualization selection
    Moved from hardcoding the buttons to generating
    them so adding new visualizations is just a matter
    of adding a new object to the BUTTONNAMES
    array constant

--- src/components/visualizationpage.tsx
+++ src/components/visualizationpage.tsx
@@ -2,8 +2,25 @@ import React, { useState } from 'react';
 import { RankedVisualizationType } from '../models/RankedVisualizationType';
 import RankedDataVisualizationFactory from './rankeddatavisualization';
 
+interface ButtonNameAndVisualization {
+    name: string,
+    visualization: RankedVisualizationType
+}
+
 const VisualizationPage = () => {
+    const BUTTONCLASS: string = "btn btn-outline-primary";
+    const BUTTONACTIVE: string = "active";
+    const LPCHANGEPERSESSION: string = "LP change per session";
+
+    const BUTTONNAMES: ButtonNameAndVisualization[] = [
+        { name: "LP change per session", visualization: RankedVisualizationType.PerSession },
+        { name: "LP change for all games", visualization: RankedVisualizationType.AllGames },
+        { name: "Win/loss percentage", visualization: RankedVisualizationType.WinLoss },
+        { name: "Character frequency", visualization: RankedVisualizationType.CharFreq }
+    ]; 
+
     const [rankedVisualizationType, setRankedVisualizationType] = useState<RankedVisualizationType>(RankedVisualizationType.PerSession);
+    const [activeButton, setActiveButton] = useState<string>(LPCHANGEPERSESSION);
 
     const changeVisualizationType = (event: React.MouseEvent<HTMLButtonElement>) => {
         let visualizationButton: HTMLButtonElement = event.currentTarget;
@@ -13,22 +30,24 @@ const VisualizationPage = () => {
         }
 
         setRankedVisualizationType(visualizationButton.value as RankedVisualizationType);
+        setActiveButton(visualizationButton.name)
     }
 
     return (
         <div className='container'>
             <div className='row m-5'>
-                <div className='col d-flex justify-content-center'>
-                    <button type="button" onClick={changeVisualizationType} value={RankedVisualizationType.PerSession} className="btn btn-primary">LP change per session</button>
-                </div>
-                <div className='col d-flex justify-content-center'>
-                    <button type="button" onClick={changeVisualizationType} value={RankedVisualizationType.AllGames} className="btn btn-primary">LP change for all games</button>
-                </div>
-                <div className='col d-flex justify-content-center'>
-                    <button type="button" onClick={changeVisualizationType} value={RankedVisualizationType.WinLoss} className="btn btn-primary">Win/loss percentage</button>
-                </div>
-                <div className='col d-flex justify-content-center'>
-                    <button type="button" onClick={changeVisualizationType} value={RankedVisualizationType.CharFreq} className="btn btn-primary">Character frequency</button>
+                <div className="btn-group" role="group">
+                    {BUTTONNAMES.map((nameAndVisual, idx) =>{
+                        return <button 
+                            type='button' 
+                            className={activeButton === nameAndVisual.name ? `${BUTTONCLASS} ${BUTTONACTIVE}` : BUTTONCLASS}
+                            onClick={changeVisualizationType}
+                            value={nameAndVisual.visualization}
+                            key={idx}
+                            name={nameAndVisual.name}>
+                                {nameAndVisual.name}
+                            </button>
+                    })}
                 </div>
             </div>
 
